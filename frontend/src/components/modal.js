0d578df4bf5fdbeea4bb67de5da3d329431e4935
Cam Way <cway140@gmail.com>
2020-12-28 19:16:05 -0800

    Fixed: wrong description showed after editing two or more items

--- frontend/src/components/modal.js
+++ frontend/src/components/modal.js
@@ -4,10 +4,11 @@ import { sendEdit } from "./../functions/todoActions";
 
 const Modal = (props) => {
   const todo = props.todo;
-  const [description, changeDescription] = useState(todo.description);
+  const [description, changeDescription] = useState("");
 
   const editTodo = () => {
     todo.description = description;
+    changeDescription("");
     sendEdit(todo);
     props.setModal();
   };
@@ -22,11 +23,14 @@ const Modal = (props) => {
       <div className="z-20 fixed bg-white rounded-lg">
         <div className="py-4 px-20">
           <h1 className="text-3xl w-full text-center">Edit Todo</h1>
-          <input
-            className="w-full p-2 pt-6 h-12 border-b text-xl outline-none"
-            placeholder={todo.description}
-            onChange={(e) => changeDescription(e.target.value)}
-          ></input>
+          <form onSubmit={() => editTodo()}>
+            <input
+              className="w-full p-2 pt-6 h-12 border-b text-xl outline-none"
+              placeholder={todo.description}
+              onChange={(e) => changeDescription(e.target.value)}
+              value={description}
+            ></input>
+          </form>
           <div className="pt-4 bottom-0 flex justify-between">
             <button
               className="p-2 w-24 m-2 border border-yellow-500 rounded-full text-yellow-500 hover:bg-yellow-500 hover:text-white"
