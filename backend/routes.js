06dafcc0589ce9a0d4056cb3d72f58e9397130ee
Cam Way <cway140@gmail.com>
2021-01-13 13:04:59 -0800

    Added: Routes for Categories

--- backend/routes.js
+++ backend/routes.js
@@ -18,14 +18,14 @@ app.get("/", (req, res) => {
 //create todo
 app.post("/todos", async (req, res) => {
   try {
-    const { description } = req.body;
+    const { description, category_id } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todos (description) VALUES ($1)",
-      [description]
+      "INSERT INTO todos (description, category_id) VALUES ($1, $2)",
+      [description, category_id]
     );
     res.json(newTodo);
   } catch (err) {
-    console.errror(err.message);
+    console.error(err.message);
   }
 });
 //get all todos
@@ -83,14 +83,14 @@ app.delete("/todos/:id", async (req, res) => {
 // add to completed
 app.post("/completed", async (req, res) => {
   try {
-    const { description } = req.body;
+    const { description, category_id } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO completed (description) VALUES ($1)",
-      [description]
+      "INSERT INTO completed (description, category_id) VALUES ($1, $2)",
+      [description, category_id]
     );
     res.json(newTodo);
   } catch (err) {
-    console.errror(err.message);
+    console.error(err.message);
   }
 });
 
@@ -112,6 +112,58 @@ app.delete("/completed/:id", async (req, res) => {
     const { id } = req.params;
     const todo = await pool.query("DELETE FROM completed WHERE id = $1", [id]);
     res.json("Todo Deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+app.get("/categories", async (req, res) => {
+  try {
+    const allCategories = await pool.query(
+      "SELECT * FROM categories ORDER BY id ASC"
+    );
+    res.json(allCategories.rows);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post("/categories", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO categories (description) VALUES ($1)",
+      [description]
+    );
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//edit a category
+app.put("/categories/:id", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const { id } = req.params;
+    await pool.query("UPDATE categories SET description = $1 WHERE id = $2", [
+      description,
+      id,
+    ]);
+    res.json("Category Updated");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+app.delete("/categories/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await pool.query("DELETE FROM categories WHERE id = $1", [
+      id,
+    ]);
+    res.json("Category Deleted");
   } catch (err) {
     console.error(err.message);
   }
